37adf749d7b4e73047fdb6a678261a21fdeb270e
udK01 <dominik.muranyi@gmail.com>
2025-05-04 20:17:01 +0100

    Added hero images.

--- mindmoss-site/src/App.jsx
+++ mindmoss-site/src/App.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="text-orange-500 text-[60px] text-center w-full bg-red-500">
-      Hello.
-    </div>
-  );
+  return <section className="bg-[#F1E9DB]"></section>;
 }
 
 export default App;
