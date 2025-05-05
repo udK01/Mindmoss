041462f8d3c25b94946d59d79dc7eeae3b2d0b6c
udK01 <dominik.muranyi@gmail.com>
2025-05-05 20:15:09 +0100

    Went down from Tailwind 4 to 3.

--- mindmoss-site/src/App.jsx
+++ mindmoss-site/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function App() {
-  return <section className="bg-[#F1E9DB]"></section>;
+  return (
+    <section className="text-[64px] bg-primary text-orange-500">asdasd</section>
+  );
 }
 
 export default App;
