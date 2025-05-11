d176da69f29d4a29996fbb22ad673b262460a1cb
udK01 <dominik.muranyi@gmail.com>
2025-05-11 13:19:41 +0100

    Added animation to menu icon.

--- mindmoss-site/src/components/Header.jsx
+++ mindmoss-site/src/components/Header.jsx
@@ -1,9 +1,30 @@
-import { IoMdMenu } from "react-icons/io";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="fixed top-4 right-0 flex items-center justify-end text-white text-[24px] px-[32px] z-50 w-full">
-      <IoMdMenu className="text-dark_gray text-[48px] cursor-pointer" />
+    <div className="fixed top-8 right-4 flex items-center justify-end text-white px-[32px] z-50 w-full">
+      <div
+        className="w-[40px] h-[30px] flex flex-col justify-between cursor-pointer group"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span
+          className={`h-[4px] bg-dark_gray transition-all duration-300 group-hover:bg-grass ${
+            isOpen ? "rotate-45 translate-y-[13px]" : ""
+          }`}
+        />
+        <span
+          className={`h-[4px] bg-dark_gray transition-all duration-300 group-hover:bg-grass ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
+        />
+        <span
+          className={`h-[4px] bg-dark_gray transition-all duration-300 group-hover:bg-grass ${
+            isOpen ? "-rotate-45 -translate-y-[13px]" : ""
+          }`}
+        />
+      </div>
     </div>
   );
 }
