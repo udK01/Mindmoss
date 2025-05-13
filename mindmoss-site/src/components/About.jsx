fc9c51c5c4b37e5499d8ecd1dc8a35cbec68b43b
udK01 <dominik.muranyi@gmail.com>
2025-05-13 10:41:52 +0100

    Initialised new classes.

---
+++ mindmoss-site/src/components/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section>
+      {/* Title */}
+      <div className="relative flex items-center justify-center w-fit px-28 -ml-10">
+        <div className="font-finger text-[72px] text-beige z-10">
+          Who are we?
+        </div>
+        <img src="./BrushStrokes/Title.png" className="absolute object-cover" />
+      </div>
+    </section>
+  );
+}
