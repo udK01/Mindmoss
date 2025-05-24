2a901b2c9211e2bec0848ca67893dd3a732e5690
udK01 <dominik.muranyi@gmail.com>
2025-05-24 19:40:42 +0100

    Added responsiveness to explore games text.

--- mindmoss-site/src/components/ExploreOurGames.jsx
+++ mindmoss-site/src/components/ExploreOurGames.jsx
@@ -5,7 +5,7 @@ export default function ExploreOurGames() {
     <section className="bg-grass w-full h-full space-y-80 overflow-hidden">
       <div className="relative flex flex-col items-center z-10">
         {/* Text */}
-        <div className="font-rubikWet text-[120px] text-grass text-center z-10">
+        <div className="font-rubikWet 2xs:text-[40px] sm:text-[70px] md:text-[100px] xl:text-[120px] text-grass text-center z-10">
           Explore Our Games
         </div>
 
