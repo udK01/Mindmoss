b74d7eefe6e88b6045c37ebf9d76b32391a8a052
udK01 <dominik.muranyi@gmail.com>
2025-05-25 18:19:24 +0100

    Added paint drip background.

--- mindmoss-site/src/components/ExploreOurGames.jsx
+++ mindmoss-site/src/components/ExploreOurGames.jsx
@@ -2,20 +2,25 @@ import Carousel from "./Carousel";
 
 export default function ExploreOurGames() {
   return (
-    <section className="bg-grass w-full h-full space-y-80 overflow-hidden">
-      <div className="relative flex flex-col items-center z-10">
+    <section className="bg-grass relative w-full h-full overflow-hidden">
+      {/* Background Image */}
+      <img
+        src="./PaintDrip.png"
+        alt="Paint Drip"
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+      />
+
+      {/* Content Wrapper */}
+      <div className="relative z-10 flex flex-col items-center space-y-40">
         {/* Text */}
-        <div className="font-rubikWet 2xs:text-[40px] sm:text-[70px] md:text-[100px] xl:text-[120px] text-grass text-center z-10">
+        <div className="font-rubikWet text-center text-grass 2xs:text-[40px] sm:text-[70px] md:text-[100px] xl:text-[120px]">
           Explore Our Games
         </div>
 
-        {/* Image below the text */}
-        {/* Image */}
-        <div className="absolute w-full h-[300px] bg-gray-500" />
-      </div>
-
-      <div className="py-40 px-40">
-        <Carousel />
+        {/* Carousel */}
+        <div className="px-10 sm:px-20 md:px-40 py-20 w-full">
+          <Carousel />
+        </div>
       </div>
     </section>
   );
