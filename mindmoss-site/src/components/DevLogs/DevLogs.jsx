0e49ebaa2d5e2353f431c21126c5d443c56181e2
udK01 <dominik.muranyi@gmail.com>
2025-06-02 18:49:09 +0100

    Added filter icon.

--- mindmoss-site/src/components/DevLogs/DevLogs.jsx
+++ mindmoss-site/src/components/DevLogs/DevLogs.jsx
@@ -1,8 +1,13 @@
+import { FaFilter } from "react-icons/fa";
+
 import Log from "./Log";
 
 export default function DevLogs() {
   return (
-    <section className="min-h-screen flex justify-center py-[200px] bg-beige">
+    <section className="min-h-screen flex flex-col justify-center items-center py-[200px] bg-beige">
+      <div className="w-[60%] py-10 flex justify-end">
+        <FaFilter className="size-10 hover:cursor-pointer hover:text-grass transition-all duration-300" />
+      </div>
       <div className="w-[80%] flex flex-col space-y-10 items-center">
         <Log />
         <Log />
