1855bc4895b157fd0fb97b3be716cf9005d6e1b3
udK01 <dominik.muranyi@gmail.com>
2025-06-09 19:07:49 +0100

    Got rid off excess lines.

--- mindmoss-site/src/components/Home.jsx
+++ mindmoss-site/src/components/Home.jsx
@@ -6,8 +6,6 @@ export default function Home() {
     <section className="overflow-x-hidden">
       <Hero />
       <ExploreOurGames />
-      {/* Background */}
-      {/* <div className="w-full h-full bg-beige absolute -z-50 inset-0" /> */}
     </section>
   );
 }
