4d2ff03b4dd81869bf23b3465c40d9c631704818
udK01 <dominik.muranyi@gmail.com>
2025-07-08 22:00:55 +0100

    Adjusted drip.

--- mindmoss-site/src/components/Footer.jsx
+++ mindmoss-site/src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
     <section className="relative w-full h-full flex flex-col">
       <div className="absolute w-full h-full bg-grass opacity-100 z-0" />
 
-      <div className="relative w-full h-[300px] xl:h-[600px] bg-beige z-20 mask-paint bg-no-repeat bg-cover bg-bottom" />
+      <div className="relative w-full h-[300px] xl:h-[600px] bg-grass z-20 mask-paint bg-no-repeat bg-cover bg-bottom" />
 
       <div className="w-full flex justify-center text-white z-10">
         <div className="xl:w-[80%] flex lg:flex-row 2xs:flex-col justify-between items-center space-y-10 mb-10">
